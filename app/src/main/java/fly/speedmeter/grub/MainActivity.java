3302798aa827eb4f42a89dabd07bd27fbed7de78
Gabriel Ibáñez <isseigx@protonmail.ch>
2021-09-26 23:15:20 -0600

    Does not stop MainActivity init if location permission is not granted

--- app/src/main/java/fly/speedmeter/grub/MainActivity.java
+++ app/src/main/java/fly/speedmeter/grub/MainActivity.java
@@ -69,7 +69,6 @@ public class MainActivity extends AppCompatActivity implements LocationListener,
 
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1337);
-            return;
         }
 
         onGpsServiceUpdate = new Data.OnGpsServiceUpdate() {

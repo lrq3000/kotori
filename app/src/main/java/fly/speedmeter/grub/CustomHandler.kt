52582f6e2ec8002c5a7239205e784aa40ab49d58
Gabriel Ibáñez <isseigx@protonmail.ch>
2022-01-14 23:38:42 -0600

    Add custom Handler

---
+++ app/src/main/java/fly/speedmeter/grub/CustomHandler.kt
@@ -0,0 +1,16 @@
+package fly.speedmeter.grub
+
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
+
+class CustomHandler(callback: (msg: Message) -> Unit) : Handler(Looper.getMainLooper()) {
+    private val mCallback = callback
+
+    override fun handleMessage(msg: Message) {
+        when (msg.what) {
+            REGISTER, UNREGISTER, DATA_UPDATE, RUNNING_UPDATE, RESET, GPS_DISABLED -> mCallback.invoke(msg)
+            else -> super.handleMessage(msg)
+        }
+    }
+}

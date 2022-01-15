06d750c5f38c3494cd1f5f78c1c8bcd81b4fdd07
Gabriel Ibáñez <isseigx@protonmail.ch>
2022-01-14 23:47:48 -0600

    Add PositioningData class (formerly Data class)

---
+++ app/src/main/java/fly/speedmeter/grub/PositioningData.kt
@@ -0,0 +1,15 @@
+package fly.speedmeter.grub
+
+data class PositioningData(
+    var isRunning: Boolean = false,
+    var time: Long = 0,
+    var timeStopped: Long = 0,
+    var isFirstTime: Boolean = true,
+    var accuracy: Float = 0.0f,
+    var distance: Double = 0.0,
+    var currentSpeed: Float = 0.0f,
+    var maxSpeed: Float = 0.0f,
+    var altitude: Double = 0.0,
+    var satellitesUsed: Int = 0,
+    var satellites: Int = 0
+)

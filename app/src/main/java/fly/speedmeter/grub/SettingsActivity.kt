411c75909d8a7a0a2771541bef12f9e6e133835d
Gabriel Ibáñez <isseigx@protonmail.ch>
2022-01-15 22:59:14 -0600

    Change app theme from settings activity

--- app/src/main/java/fly/speedmeter/grub/SettingsActivity.kt
+++ app/src/main/java/fly/speedmeter/grub/SettingsActivity.kt
@@ -1,22 +1,53 @@
 package fly.speedmeter.grub
 
+import android.content.SharedPreferences
 import android.os.Bundle
 
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 
 import com.google.android.material.appbar.MaterialToolbar
 
 class SettingsActivity : AppCompatActivity() {
+    
     internal class SettingsFragment : PreferenceFragmentCompat() {
+        
+        private var themePref: Preference? = null
+        
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.pref_general, rootKey)
-
+            
+            var sharedPreferences = preferenceScreen.getSharedPreferences()
+            val currentTheme = sharedPreferences.getString("theme", "system")
+            val themeValuesArray = resources.getStringArray(R.array.theme_values)
+            val themesArray = resources.getStringArray(R.array.themes)
+            
             var pref: Preference? = findPreference("version")
 
             if (pref != null) {
                 pref.setSummary(BuildConfig.VERSION_NAME)
+            }
+            
+            themePref = findPreference("theme")
+            themePref?.setSummary(themesArray[themeValuesArray.indexOf(currentTheme)])
+            
+            themePref?.setOnPreferenceChangeListener { preference, newValue ->
+                val value = newValue as String
+                preference.setSummary(themesArray[themeValuesArray.indexOf(value)])
+                when (value) {
+                    "light" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    "dark" -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    else -> {
+                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.P) {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                        } else {
+                            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY)
+                        }
+                    }
+                }
+                true
             }
         }
     }

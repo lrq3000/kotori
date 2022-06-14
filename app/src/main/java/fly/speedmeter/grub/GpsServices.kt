7eed91996d2e79d28b7d83c5fad6540349ed07bd
Gabriel Ibáñez <isseigx@protonmail.ch>
2022-06-13 18:02:13 -0600

    Call method available from api level 24

--- app/src/main/java/fly/speedmeter/grub/GpsServices.kt
+++ app/src/main/java/fly/speedmeter/grub/GpsServices.kt
@@ -98,9 +98,9 @@ class GpsServices : Service(), LocationListenerCompat {
         )
 
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
-            mLocationManager.addNmeaListener(ContextCompat.getMainExecutor(this), {
-                message, _ -> onNmeaMessage(message)
-            })
+            mLocationManager.addNmeaListener({
+            	message, _ -> onNmeaMessage(message)
+            }, null)
         }
     }
 

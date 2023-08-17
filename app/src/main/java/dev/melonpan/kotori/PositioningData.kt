8478b43d1db2623dc6e546ffc8c53c570df41332
Gabriel Ibáñez <isseigx@protonmail.ch>
2023-08-16 19:34:57 -0600

    Order properties alphabetically.

--- app/src/main/java/dev/melonpan/kotori/PositioningData.kt
+++ app/src/main/java/dev/melonpan/kotori/PositioningData.kt
@@ -1,16 +1,16 @@
 package dev.melonpan.kotori
 
 data class PositioningData(
-    var isRunning: Boolean = false,
-    var time: Long = 0,
-    var timeStopped: Long = 0,
-    var isFirstTime: Boolean = true,
     var accuracy: Float = 0.0f,
-    var distance: Double = 0.0,
-    var currentSpeed: Float = 0.0f,
-    var maxSpeed: Float = 0.0f,
     var altitude: Double = 0.0,
     var altitudeMeanSeaLevel: Double = 0.0,
+    var currentSpeed: Float = 0.0f,
+    var distance: Double = 0.0,
+    var isFirstTime: Boolean = true,
+    var isRunning: Boolean = false,
+    var maxSpeed: Float = 0.0f,
     var satellitesUsed: Int = 0,
-    var satellites: Int = 0
+    var satellites: Int = 0,
+    var time: Long = 0,
+    var timeStopped: Long = 0
 )
